c3646c83718bc8da1b905c4f1dbdc68f02fac13b
Leeyoungsup <liive29252@gmail.com>
2023-01-27 15:40:35 +0900

    Deps

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -4,10 +4,19 @@ function App() {
   const [keyword, setKeyword] = useState('');
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
-  console.log('i run all the time');
+
   React.useEffect(() => {
-    console.log('Call the Api');
+    console.log('I run only once');
   }, []);
+  React.useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  }, [keyword]);
+  React.useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]);
+  React.useEffect(() => {
+    console.log("I run when 'counter' & 'keyword' changes");
+  }, [keyword, counter]);
   return (
     <div>
       <input
