1c62242558001c9bf5c261aa7743469145a10f71
Leeyoungsup <liive29252@gmail.com>
2023-01-27 14:56:23 +0900

    11

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,15 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 function App() {
   const [counter, setValue] = React.useState(0);
+  const [keyword, setKeyword] = useState('');
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log('render11');
   const iRunOnlyOnce = () => {
     console.log('render');
   };
-
+  console.log('SEARCH FOR', keyword);
   useEffect(iRunOnlyOnce, []);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="search here...."
+      ></input>
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
